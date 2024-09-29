dcab32b7a9844cf0229a72825807def2ce255080
Alexis <amaryllisno@gmail.com>
2024-09-30 01:59:30 +0200

    added audio to shadow puzzle

--- js/events/launch.js
+++ js/events/launch.js
@@ -7,13 +7,15 @@ export let hasLaunched = false;
 // Find the page with matching pathname
 const currentPage = pages.find((page) => page.pathname === filteredURL);
 
-let bgMusic;
+let bgMusic = null;
 
 if (currentPage && currentPage.music) {
   // Initialize the audio if music exists for this page
   bgMusic = new Audio(currentPage.music);
   bgMusic.volume = 0.6;
 }
+
+export const currentBgMusic = bgMusic;
 
 launch.addEventListener('click', () => {
   // hide and remove cover
--- js/pages/tribulation.js
+++ js/pages/tribulation.js
@@ -1,11 +1,58 @@
 import '../events/launch.js';
+import { currentBgMusic } from '../events/launch.js';
 
 const container = document.querySelector('#tribulation-container');
 const overlay = document.querySelector('.dark-overlay');
 
+let alertTimeout = null; // To store the timeout ID
+
+const updatePosition = (x, y) => {
+  const vw = window.innerWidth;
+  const vh = window.innerHeight;
+
+  const mouseX = x / vw;
+  const mouseY = y / vh;
+
+  const currentFrameX = Math.floor(mouseX * 24);
+  const currentFrameY = Math.floor(mouseY * 24);
+  console.log(currentFrameX, currentFrameY);
+
+  // Check if hovering over the special frame (6, 6)
+  if (currentFrameX === 11 && currentFrameY === 14) {
+    if (!alertTimeout) {
+      alertTimeout = setTimeout(() => {
+        currentBgMusic.pause();
+        alert('please dont look');
+        new Audio('/media/sounds/ictnm.mp3').play();
+        console.log('currentBgMusic: ' + currentBgMusic);
+
+        console.log('...before pushing sigilState, ', sigilsState);
+        // sigilsState.push({
+        //   name: 'tribulation',
+        //   sigilImage: '/media/img/progression/2.png',
+        // });
+        // console.log('...after pushing sigilState');
+        console.log('sigilstate: ', sigilsState);
+        // localStorage.setItem('sigils', JSON.stringify(sigilsState));
+
+        alertTimeout = null; // Reset after the alert fires'
+        window.location.href = '/';
+      }, 1500);
+    }
+  } else {
+    // Cancel the alert if we move away from the special frame
+    if (alertTimeout) {
+      clearTimeout(alertTimeout);
+      alertTimeout = null; // Reset the timeout
+    }
+  }
+};
+
 document.addEventListener('mousemove', function (e) {
   const x = e.clientX; // Get the horizontal coordinate
   const y = e.clientY; // Get the vertical coordinate
+
+  updatePosition(x, y);
 
   // Update the background-image property to move the gradient center to the cursor location
   overlay.style.backgroundImage = `radial-gradient(circle at ${x}px ${y}px, 
